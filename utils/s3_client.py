d1aef8f7e530de119b83f6e8b69181ef416dd1a2
Yussdan <Yussdan@gmail.com>
2024-11-22 01:53:29 +0300

    lazy formmater

--- utils/s3_client.py
+++ utils/s3_client.py
@@ -84,5 +84,5 @@ def make_request(endpoint='', params=None, url='https://min-api.cryptocompare.co
         response.raise_for_status()
         return response.json()
     except requests.exceptions.RequestException as e:
-        logger.error(f"Request error: {e}")
+        logger.error("Request error: %s", e)
         return {"error": str(e)}
