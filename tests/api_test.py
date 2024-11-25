81bbe20acbb2e379b23df2e0aa8ba08d90bede8c
Yussdan <Yussdan@gmail.com>
2024-11-25 04:19:56 +0300

    fix

--- tests/api_test.py
+++ tests/api_test.py
@@ -1,21 +1,8 @@
-import pytest
-import requests
-
-# Модульный docstring
 """
 Тесты для API, включающие проверку получения последних данных о курсе BTC/USD
 и получения исторических данных о курсе BTC за последние 5 дней.
 """
-
-# Фикстура для подготовки базового URL API
-@pytest.fixture
-def api_base_url():
-    """
-    Фикстура, возвращающая базовый URL для тестируемого API.
-
-    Используется в тестах для получения URL для запросов.
-    """
-    return 'http://localhost:5000'
+import requests
 
 def test_get_latest(api_base_url):
     """
@@ -24,7 +11,7 @@ def test_get_latest(api_base_url):
     Отправляет GET-запрос к API, проверяет корректность статуса ответа,
     а также содержимое данных (тип данных и наличие информации о BTC).
     """
-    response = requests.get(f'{api_base_url}/latest/BTC/USD', timeout=20)
+    response = requests.get(f'http://localhost:5000/latest/BTC/USD', timeout=20)
     assert response.status_code == 200
 
     data = response.json()
@@ -39,7 +26,7 @@ def test_get_history(api_base_url):
     Отправляет GET-запрос к API, проверяет статус ответа и содержимое данных.
     Убедится, что в ответе нет ошибок.
     """
-    response = requests.get(f'{api_base_url}/history/BTC/day/USD/5', timeout=20)
+    response = requests.get(f'http://localhost:5000/history/BTC/day/USD/5', timeout=20)
     assert response.status_code == 200
 
     data = response.json()
