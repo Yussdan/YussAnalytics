374a9ff9cfa1454a56ee13b049e446aa79d41eb7
Yussdan <Yussdan@gmail.com>
2024-11-25 04:14:46 +0300

    fix linter

--- tests/api_test.py
+++ tests/api_test.py
@@ -1,8 +1,24 @@
-# test_api.py
+import pytest
 import requests
 
-def test_get_latest():
-    response = requests.get('http://localhost:5000/latest/BTC/USD')
+# Фикстура для подготовки базового URL API
+@pytest.fixture
+def api_url():
+    """
+    Фикстура, возвращающая базовый URL для тестируемого API.
+
+    Используется в тестах для получения URL для запросов.
+    """
+    return 'http://localhost:5000'
+
+def test_get_latest(api_url):
+    """
+    Тест для получения последних данных о курсе BTC/USD.
+
+    Отправляет GET-запрос к API, проверяет корректность статуса ответа,
+    а также содержимое данных (тип данных и наличие информации о BTC).
+    """
+    response = requests.get(f'{api_url}/latest/BTC/USD', timeout=20)
     assert response.status_code == 200
 
     data = response.json()
@@ -10,12 +26,17 @@ def test_get_latest():
     assert len(data) > 0
     assert 'BTC' in data
 
-def test_get_history():
-    response = requests.get('http://localhost:5000/history/BTC/day/USD/5"')
+def test_get_history(api_url):
+    """
+    Тест для получения исторических данных о курсе BTC за последние 5 дней.
+
+    Отправляет GET-запрос к API, проверяет статус ответа и содержимое данных.
+    Убедится, что в ответе нет ошибок.
+    """
+    response = requests.get(f'{api_url}/history/BTC/day/USD/5', timeout=20)
     assert response.status_code == 200
 
     data = response.json()
     assert isinstance(data, dict)
     assert len(data) > 0
     assert 'error' not in data
-
