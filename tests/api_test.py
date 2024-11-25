fb7187022dbddc33f5549f8a9cebefc3039d9852
Yussdan <Yussdan@gmail.com>
2024-11-25 04:21:36 +0300

    fix

--- tests/api_test.py
+++ tests/api_test.py
@@ -4,14 +4,14 @@
 """
 import requests
 
-def test_get_latest(api_base_url):
+def test_get_latest():
     """
     Тест для получения последних данных о курсе BTC/USD.
 
     Отправляет GET-запрос к API, проверяет корректность статуса ответа,
     а также содержимое данных (тип данных и наличие информации о BTC).
     """
-    response = requests.get(f'http://localhost:5000/latest/BTC/USD', timeout=20)
+    response = requests.get('http://localhost:5000/latest/BTC/USD', timeout=20)
     assert response.status_code == 200
 
     data = response.json()
@@ -19,14 +19,14 @@ def test_get_latest(api_base_url):
     assert len(data) > 0
     assert 'BTC' in data
 
-def test_get_history(api_base_url):
+def test_get_history():
     """
     Тест для получения исторических данных о курсе BTC за последние 5 дней.
 
     Отправляет GET-запрос к API, проверяет статус ответа и содержимое данных.
     Убедится, что в ответе нет ошибок.
     """
-    response = requests.get(f'http://localhost:5000/history/BTC/day/USD/5', timeout=20)
+    response = requests.get('http://localhost:5000/history/BTC/day/USD/5', timeout=20)
     assert response.status_code == 200
 
     data = response.json()
